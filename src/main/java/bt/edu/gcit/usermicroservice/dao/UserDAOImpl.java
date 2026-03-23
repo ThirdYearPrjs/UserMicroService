05652e69ee509ed93bd9e22c7541dff9652ceb02
Lungten <12230028.gcit@rub.edu.bt>
2026-03-23 07:03:25 +0000

    user crud done with image upload

--- src/main/java/bt/edu/gcit/usermicroservice/dao/UserDAOImpl.java
+++ src/main/java/bt/edu/gcit/usermicroservice/dao/UserDAOImpl.java
@@ -4,6 +4,9 @@ import bt.edu.gcit.usermicroservice.entity.User;
 import org.springframework.stereotype.Repository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import java.util.List;
+import bt.edu.gcit.usermicroservice.exception.UserNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -21,4 +24,47 @@ public class UserDAOImpl implements UserDAO {
     public User save(User user) {
         return entityManager.merge(user);
     }
+
+    @Override
+    public User findByEmail(String email) {
+        TypedQuery<User> query = entityManager.createQuery("from User where email = :email", User.class);
+        query.setParameter("email", email);
+        List<User> users = query.getResultList();
+        System.out.println(users.size());
+        if (users.isEmpty()) {
+            return null;
+        } else {
+            System.out.println(users.get(0) + " " + users.get(0).getEmail());
+            return users.get(0);
+        }
+    }
+
+    @Override
+    public User findByID(int theId) {
+        // Implement the logic to find a user by their ID in the database
+        // and return the user object
+        User user = entityManager.find(User.class, theId);
+        return user;
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        // Implement the logic to delete a user by their ID from the database
+        // find user by id
+        User user = findByID(theId);
+        // remove user
+        entityManager.remove(user);
+    }
+
+    @Override
+    public void updateUserEnabledStatus(int id, boolean enabled) {
+        User user = entityManager.find(User.class, id);
+        System.out.println(user);
+        if (user == null) {
+            throw new UserNotFoundException("User not found with id " + id);
+        }
+        user.setEnabled(enabled);
+        entityManager.persist(user);
+    }
+
 }
